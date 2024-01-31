fe418846a46bc93b8c86405919053a327fca9d38
nik <yarik200003@gmail.com>
2024-01-31 14:00:17 +0500

    second page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Page() {
+    return (
+        <main>
+            <div className="flex place-content-center h-[calc(100vh-12rem)]">
+                <div className="m-auto">
+                    <p className="text-xl text-center">хихиХАХА</p>
+                </div>
+            </div>
+        </main>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        'custom': '0 0 50px -10px rgb(0 0 0 / 0.25);',
+      }
     },
   },
   plugins: [],
