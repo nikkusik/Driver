a04089f8cbc93ddec84e196dd4a4767f4cfdbba9
nik <yarik200003@gmail.com>
2024-01-31 14:01:29 +0500

    second page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
         <main>
             <div className="flex place-content-center h-[calc(100vh-12rem)]">
                 <div className="m-auto">
-                    <p className="text-xl text-center">хихиХАХА</p>
+                    <p className="text-xl text-center">Страница для проверки переходов</p>
                 </div>
             </div>
         </main>
