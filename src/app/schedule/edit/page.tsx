556a183edd42ff7a4e23a507c5fed82e58fc0241
nikkusik <yarik200003@yandex.ru>
2024-06-09 00:59:05 +0500

    add loading

--- src/app/schedule/edit/page.tsx
+++ src/app/schedule/edit/page.tsx
@@ -1,10 +1,8 @@
-// pages/schedule/edit/page.tsx
 'use client';
 
 import { useRouter, useSearchParams } from 'next/navigation';
 import React, { ChangeEvent, FormEvent, useEffect, useState, Suspense } from 'react';
 import { update } from '@/app/api/schedule';
-import Loading from '@/components/Loading';
 
 function ScheduleForm({ id, initStudent, initDriver, initCar, initStartdatetime }: any) {
 
